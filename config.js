ed11a3317676c2629c29c4c40ab2c4a6ff210ef6
uruguayunicampo-crypto <uruguayunicampo@gmail.com>
2026-03-31 09:18:00 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ window.UNICAMPO_CONFIG = {
   sheetName: 'Biblioteca',
   usuarios: {
     'admin':     { pass: 'Unicampo2024!', nombre: 'Administrador', foto: '' },
-    'nico':      { pass: 'nicoensayos.',  nombre: 'Ing. Agr.(Msc.) Nicolas Cortazzo', foto: 'https://URL-FOTO-NICO.jpg' },
+    'nico':      { pass: 'nicoensayos.',  nombre: 'Ing. Agr.(Msc.) Nicolas Cortazzo', foto: 'https://drive.google.com/thumbnail?id=1VZ9ioeNIBsVgHxMo-7HUAzlITBZ3SxN6' },
     'tato':      { pass: 'tatoensayos.',  nombre: 'Ing. Agr. Esteban Hoffman Berasain', foto: 'https://drive.google.com/thumbnail?id=1ScLgILo4-W3vC4vp0PYKHDdEp-2mfwjU&sz=w100' },
-    'alejandra': { pass: 'alensayos.',    nombre: 'Ing. Agr.(Msc.) Alejandra Vulliez', foto: 'https://URL-FOTO-ALE.jpg' },
+    'alejandra': { pass: 'alensayos.',    nombre: 'Ing. Agr.(Msc.) Alejandra Vulliez', foto: 'https://drive.google.com/thumbnail?id=1VZ9ioeNIBsVgHxMo-7HUAzlITBZ3SxN6' },
   }
 };
